0f90fe896dfcc0fbcdcb8b677584f998820a7ed0
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2022-07-28 11:32:39 -0300

    initial commit - GameBoard component

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="board">GameBoard</div>
+</template>
+
+<script>
+export default {
+    name: "GameBoard",
+    props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped></style>
